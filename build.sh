b2e7340ee47da5bc542868e9a99919786938f886
centrix <859769033@qq.com>
2023-05-15 22:01:08 +0800

    fix x-pack and starlight version problem

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 ./gradlew localDistro
 cp -f modified/brpc-spring-boot-starter-3.0.5.jar build/distribution/local/elasticsearch-8.6.0-SNAPSHOT/lib
-cp -f modified/spring-cloud-baidu-thirdparty-commons-2022.2.0-SNAPSHOT.jar build/distribution/local/elasticsearch-8.6.0-SNAPSHOT/lib
+cp -f modified/spring-cloud-baidu-thirdparty-commons-2022.2.0.jar build/distribution/local/elasticsearch-8.6.0-SNAPSHOT/lib
 cp -f modified/java.policy build/distribution/local/elasticsearch-8.6.0-SNAPSHOT/jdk/conf/security
